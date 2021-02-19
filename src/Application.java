0e7bfd797510c7f2f99cf89391c22ac8a4b0f887
MartaMariz <61186044+MartaMariz@users.noreply.github.com>
2021-02-19 19:02:43 +0000

    New operation new branch

--- src/Application.java
+++ src/Application.java
@@ -5,9 +5,15 @@ public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String operation = scanner.nextLine();
+
         int a = scanner.nextInt();
         int b = scanner.nextInt();
+        if (operation.equals("sum"))
+            System.out.println(a + b);
+        else if (operation.equals("mul"))
+            System.out.println(a * b);
 
-        System.out.println(a + b);
+
     }
 }
